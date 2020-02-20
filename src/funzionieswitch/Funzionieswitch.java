b4f56b1cb99ba6d9f373ddf8671b2d64b4730568
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2020-02-20 08:58:57 +0100

    introduzione
    creato il codice, da completare. è ad un buon 10%, lol

---
+++ src/funzionieswitch/Funzionieswitch.java
@@ -0,0 +1,25 @@
+
+package funzionieswitch;
+import java.util.Scanner;
+
+public class Funzionieswitch 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        Scanner kekkeroni = new Scanner(System.in);
+        int non=3, mese=0;
+        switch(non)
+        {
+            case 1: mese =1;
+            case 2: mese=2;
+            case 3: mese=3;
+        }
+        System.out.print(mese);
+        
+        
+        
+    }
+    
+}
